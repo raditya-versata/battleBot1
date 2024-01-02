9b3f6481e43ea222ddf232d367520daeeda2c2af
Raditya-versata <raditya.perdhevi@trilogy.com>
2024-01-02 15:41:25 +0700

    add load_articles for MD files

--- embeds/load_articles.py
+++ embeds/load_articles.py
@@ -1,21 +1,22 @@
+from langchain.text_splitter import MarkdownHeaderTextSplitter
+from langchain.document_loaders import UnstructuredMarkdownLoader
 from pymongo import MongoClient
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import MongoDBAtlasVectorSearch
 from langchain.document_loaders import DirectoryLoader
 from key_helper import key_helper
-import json
 
 keys = key_helper()
 client = MongoClient(keys.mongo_uri())
 db_name = "battleBot0"
-collection_name = "articles"
+collection_name = "articles_md"
 collection = client[db_name][collection_name]
 
-loader = DirectoryLoader("../_process", glob="./*.txt", show_progress=True)
+loader = DirectoryLoader("../_output/articles", glob="./*.md", show_progress=True, loader_cls=UnstructuredMarkdownLoader)
 data = loader.load()
+
 
 embeddings = OpenAIEmbeddings(openai_api_key=keys.openai_key())
 
-# json.dump(embeddings, open("result.json","w", encoding="utf-8"))
 vectorStore = MongoDBAtlasVectorSearch.from_documents(data, embeddings, collection=collection)
 
